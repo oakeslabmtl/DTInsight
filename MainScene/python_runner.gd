3122aa6292c4ba103c97ac78123e00fca34b6cea
paul-moreau <112432533+paul-moreau@users.noreply.github.com>
2026-01-11 19:53:40 -0500

    Add a popup window to select a Python script to execute when "Call a script" is pressed
    It still needs some graphics improvements

--- MainScene/python_runner.gd
+++ MainScene/python_runner.gd
@@ -1,18 +1,7 @@
 extends Button
 
-# Path of the script
-var script_path = "script_test.py" 
+@onready var script_runner_panel: Control = $"../../../ScriptRunnerBackground"
 
-# Execute a Python script when the button is pressed
 func _on_pressed():
-	if not FileAccess.file_exists(script_path):
-		printerr("Error : Python file not found at : ", script_path)
-		return
-	var command = "python" 
-	var args = [script_path]
-	var output = []	
-	var exit_code = OS.execute(command, args, output, true)	
-	if exit_code == 0:
-		print("Script successfully executed", output)
-	else:
-		printerr("Python error (Code %s) : " % exit_code, output)
+	script_runner_panel.visible = true
+	script_runner_panel.move_to_front()
